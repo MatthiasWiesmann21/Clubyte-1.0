6218a3455af8938b716f842ff01da3fe8b54b7b0
MatthiasWiesmann21 <matthiaswiesmann21@outlook.com>
2024-09-06 21:02:52 +0200

    potenital fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,6 @@
-import NextAuth, { SessionStrategy } from "next-auth";
-import { PrismaClient } from "@prisma/client";
+import NextAuth from "next-auth";
 import authOptions from "@/lib/auth";
 
-export const handlers = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
-// export const { GET, POST } = handlers
-export { handlers as GET, handlers as POST };
+export { handler as GET, handler as POST };
