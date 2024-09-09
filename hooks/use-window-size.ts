3a820d49b59b6029f49a83590d3d2d5bbb8cb901
MatthiasWiesmann21 <matthiaswiesmann21@outlook.com>
2024-09-09 21:53:52 +0200

    idk

--- hooks/use-window-size.ts
+++ hooks/use-window-size.ts
@@ -4,10 +4,6 @@ import { useEffect, useState } from "react";
 export default function useWindowSize() {
   const [windowSize, setWindowSize] = useState(() => {
     if (isServer()) return { width: 0, height: 0 };
-    // rola dhola
-    if (typeof window === "undefined") 
-      return { width: 0, height: 0 }
-
     return {
       width: window.innerWidth,
       height: window.innerHeight,
@@ -15,19 +11,12 @@ export default function useWindowSize() {
   });
 
   useEffect(() => {
-    
     const handleResize = () => {
-      if (typeof window === "undefined") 
-        return ;
-  
       setWindowSize({
         width: window.innerWidth,
         height: window.innerHeight,
       });
     };
-    if (typeof window === "undefined") 
-      return ;
-
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
