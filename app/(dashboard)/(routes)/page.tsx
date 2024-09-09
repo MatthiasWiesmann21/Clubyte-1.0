d2c8d596f7f24194b4c11980ec3f3388458f1155
MatthiasWiesmann21 <matthiaswiesmann21@outlook.com>
2024-09-09 15:21:04 +0200

    remove unused imports

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,9 +1,6 @@
 "use client"
-import { initialProfile } from "@/lib/initial-profile";
-import { getServerSession } from "next-auth/next";
+
 import { redirect } from "next/navigation";
-import authOptions from "@/lib/auth"; // Adjust the import path as needed
-import { useEffect } from "react";
 
 export default function Home() {
  return redirect("/dashboard");
