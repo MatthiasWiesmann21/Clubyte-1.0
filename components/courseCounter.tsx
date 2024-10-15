efc1da1e7918d3c55e53ddddb7180e32cf7b4a98
MatthiasWiesmann <matthiaswiesmann21@outlook.com>
2024-10-15 13:44:13 +0200

    some mobile design adjustment & document hub adjustments

--- components/courseCounter.tsx
+++ components/courseCounter.tsx
@@ -37,17 +37,17 @@ export const CourseCounter = ({
       <div className="text-prima flex w-full items-center rounded-full border border-black p-3 text-center text-sm text-slate-400 dark:border-white">
         <div className="space-between flex items-center">
           <Info className="mr-2 h-5 w-5" />
-          <p className="text-md sm:text-xs">
+          <p className="text-md xs:text-xs text-start">
             {currentLanguage.search_courseCounter_currentCourses} 
           </p>
-          <p className="ml-1 sm:text-xs">
+          <p className="ml-1 w-16 xs:text-xs">
             {courses} / {maxCourseDisplay}
           </p>
         </div>
         <div className="ml-auto">
           {isFrontend && (
             <Button
-              className="rounded-3xl text-sm"
+              className="rounded-3xl text-xs text-start"
               variant="outline"
               onClick={() => onOpen("createCourse")}
             >
