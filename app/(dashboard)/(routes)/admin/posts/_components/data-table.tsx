74036ebfa981e410dcd95aafc52cc5a9fc4dca8c
MatthiasWiesmann21 <matthiaswiesmann21@outlook.com>
2024-11-07 18:41:25 +0100

    adding clientAdmin actions to chapters and small design adjustments

--- app/(dashboard)/(routes)/admin/posts/_components/data-table.tsx
+++ app/(dashboard)/(routes)/admin/posts/_components/data-table.tsx
@@ -71,7 +71,7 @@ export function DataTable<TData, TValue>({
           onChange={(event) =>
             table.getColumn("title")?.setFilterValue(event.target.value)
           }
-          className="max-w-sm mr-3 border-[#000000] dark:border-[#ffffff]"
+          className="max-w mr-3 border-[#000000] dark:border-[#ffffff]"
         />
         {isAdmin || isClientAdmin && (
           <Button className="w-64"  onClick={() => onOpen("createPost")}>
